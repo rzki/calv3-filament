edd7cf847a80e8b2e408b453510196de8c45fa84
Rizky Dhani <rizkydhani15@gmail.com>
2024-07-07 11:26:16 +0700

    added migrations

---
+++ database/migrations/2024_01_09_105544_create_inventories_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('inventories', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('deviceId');
+            $table->foreignId('name_id')->nullable()->constrained('device_names', 'id', 'name_id')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->string('inv_number')->nullable();
+            $table->string('brand')->nullable();
+            $table->string('type')->nullable();
+            $table->string('serial_number')->nullable();
+            $table->string('location')->nullable();
+            $table->year('procurement_year')->nullable();
+            $table->string('pic')->nullable();
+            $table->foreignId('hospital_id')->nullable()->constrained('hospitals', 'id', 'hospital_id')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->date('calibration_date')->nullable();
+            $table->date('next_calibration_date')->nullable();
+            $table->string('certif_no')->nullable();
+            $table->text('certif_file')->nullable();
+            $table->text('barcode')->nullable();
+            $table->string('status')->nullable();
+            $table->foreignId('user_id')->constrained('users', 'id', 'user_id')->cascadeOnUpdate()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('devices');
+    }
+};
